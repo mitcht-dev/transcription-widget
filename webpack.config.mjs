24327341b6771a719d80a9b4afc364b44b16ec89
Mitch <238069586+mitcht-dev@users.noreply.github.com>
2026-06-18 10:29:03 -0700

    try production mode

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'index.js',
