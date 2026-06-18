1797899323c137d7eccb56c01c4c46cbcaaa3436
Mitch <238069586+mitcht-dev@users.noreply.github.com>
2026-06-17 19:03:06 -0700

    rearranging

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const platformClient = require('platformClient');
+
 const clientId = "85c16c77-dca7-4d60-b67a-6f09658aa043";
 const redirectUri = 'https://mitcht-dev.github.io/transcription-widget/';
 
